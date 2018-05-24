45c3c3f3be3c9b1a146de1bb6f1f0fb31e28ee8c
Balazs Dianiska <balazs.dianiska@gmail.com>
2018-05-24 15:43:37 +0200

    Added tests for the version evaluator helper functions.

--- compare.go
+++ compare.go
@@ -2,17 +2,15 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 // Possible outcome of a version comparison.
-// When a stable release is available, but we using an earlier, or dev version.
-const STABLE_AVAILABLE = 0
-
-// When both modules are stable and the current one is outdated.
-const OUTDATED = 1
-
-// When the module is up to date.
-const UPDATED = 2
+const (
+	DEV_STABLE_AVAILABLE   = 0 // Stable release is available (when using a dev)
+	STABLE_STATUS_OUTDATED = 1 // Both modules are stable and the current one is outdated.
+	STABLE_STATUS_UPDATED  = 2 // When the module is up to date.
+)
 
 // Check each of the manifest elements against the release data.
 func (M Manifest) compare() {
@@ -26,11 +24,11 @@ func (C Component) checkUpdate() {
 	release := fetchRelease(C.Name, C.Version.Major)
 	status := C.checkUpdateStatus(release.Releases[0])
 	switch status {
-	case UPDATED:
+	case STABLE_STATUS_UPDATED:
 		fmt.Printf("[OK]\t%s", C.Name)
-	case OUTDATED:
+	case STABLE_STATUS_OUTDATED:
 		fmt.Printf("[UPDATE AVAILABLE]\t%s", C.Name)
-	case STABLE_AVAILABLE:
+	case DEV_STABLE_AVAILABLE:
 		fmt.Printf("[STABLE AVAILABLE]\t%s", C.Name)
 	}
 	fmt.Printf("\tActual: %v\tCurrent: %v\n", C.Version, release.Releases[0])
@@ -38,14 +36,37 @@ func (C Component) checkUpdate() {
 
 func (C Component) checkUpdateStatus(r Release) int {
 	if r.Patch > C.Version.Patch {
-		return OUTDATED
+		return STABLE_STATUS_OUTDATED
 		// fmt.Printf("Component %s is outdated %v => %v\n", C.Name, C.Version, latestRelease)
 	} else if r.Minor == C.Version.Minor && r.Patch == C.Version.Patch {
-		return UPDATED
+		return STABLE_STATUS_UPDATED
 	} else if C.Version.Patch == -1 && C.Version.Minor == r.Minor {
-		return STABLE_AVAILABLE
+		return DEV_STABLE_AVAILABLE
 	}
 	return -1
+}
+
+func (C Component) isGit() bool {
+	return C.Version.Tag == "git"
+}
+
+// Non-stable, but fixed release.
+func (C Component) isBeta() bool {
+	fixedTags := []string{"rc", "beta", "alpha"}
+	for _, s := range fixedTags {
+		if strings.Index(C.Version.Tag, s) == 0 {
+			return true
+		}
+	}
+	return false
+}
+
+func (C Component) isDev() bool {
+	return C.Version.Tag == "dev"
+}
+
+func (C Component) isStable() bool {
+	return C.Version.Tag == ""
 }
 
 // Compare two versions and return a status evaluation.
---
+++ compare_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"testing"
+)
+
+var testStableVersion = Component{"module", "views", SemVersion{7, 3, 4, ""}}
+var testDevVersion = Component{"module", "views", SemVersion{7, 3, -1, "dev"}}
+var testGitVersion = Component{"module", "views", SemVersion{7, 3, -1, "git"}}
+var testBetaVersionList = []Component{
+	Component{"module", "views", SemVersion{7, 3, 4, "rc1"}},
+	Component{"module", "views", SemVersion{7, 3, 4, "beta5"}},
+	Component{"module", "views", SemVersion{7, 3, 4, "alpha5"}},
+}
+
+func TestIsGit(t *testing.T) {
+	if ok := testGitVersion.isGit(); !ok {
+		t.Error("For", testGitVersion.Version, "expected", true, "got", ok)
+	}
+}
+
+func TestIsBeta(t *testing.T) {
+	for _, v := range testBetaVersionList {
+		if ok := v.isBeta(); !ok {
+			t.Error("For", v.Version, "expected", true, "got", ok)
+		}
+	}
+
+	if ok := testGitVersion.isBeta(); ok { // A negative case.
+		t.Error("For", testGitVersion.Version, "expected", false, "got", ok)
+	}
+}
+
+func TestIsDev(t *testing.T) {
+	if ok := testDevVersion.isDev(); !ok {
+		t.Error("For", testDevVersion.Version, "expected", true, "got", ok)
+	}
+	if ok := testGitVersion.isStable(); ok {
+		t.Error("For", testGitVersion.Version, "expected", false, "got", ok)
+	}
+}
+
+func TestIsStable(t *testing.T) {
+	if ok := testStableVersion.isStable(); !ok {
+		t.Error("For", testStableVersion.Version, "expected", true, "got", ok)
+	}
+	if ok := testGitVersion.isStable(); ok {
+		t.Error("For", testGitVersion.Version, "expected", false, "got", ok)
+	}
+}

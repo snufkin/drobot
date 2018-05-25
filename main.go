49709b451733e7a0cad446e63b73ae7dbf2f3fc3
Balazs Dianiska <balazs.dianiska@gmail.com>
2018-05-25 15:28:31 +0200

    Adding some CLI support via go-commander.

--- main.go
+++ main.go
@@ -1,11 +1,40 @@
 package main
 
-func main() {
-	manifest := Manifest{}
-	manifest.parseMakefile("test/test.make")
-	manifest.compare()
+import (
+	"github.com/yitsushi/go-commander"
+)
 
-	// for _, c := range manifest.Components {
-	// fetchRelease(c.Name, c.Version.Major)
-	// }
+func main() {
+	registry := commander.NewCommandRegistry()
+	registry.Register(ParseMakefile)
+	registry.Execute()
+
+}
+
+// Parse struct is a representation of the Parse command.
+type Parse struct {
+}
+
+// Execute is the main function, called on the parse-make command.
+func (c *Parse) Execute(opts *commander.CommandHelper) {
+	manifest := Manifest{}
+	fileName := opts.Arg(0)
+	// Todo validate the file.
+	manifest.parseMakefile(fileName)
+	manifest.compare()
+}
+
+func ParseMakefile(appName string) *commander.CommandWrapper {
+	return &commander.CommandWrapper{
+		Handler: &Parse{},
+		Help: &commander.CommandDescriptor{
+			Name:             "parse",
+			ShortDescription: "Parse a Drush makefile",
+			LongDescription:  `Parse a Drush makefile (composer.lock file is not supported)`,
+			Arguments:        "<filename>",
+			Examples: []string{
+				"mysite.make",
+			},
+		},
+	}
 }
